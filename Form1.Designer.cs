0084c771cb92b025c474d92dd775fe77be587b74
Vo Anh Vu <voanhvummo@gmail.com>
2025-11-29 11:57:11 +0700

    Initial commit: upload existing StudentManagerr project

---
+++ Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿using System;
+
+namespace StudentManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblname = new System.Windows.Forms.Label();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.cboGender = new System.Windows.Forms.ComboBox();
+            this.lblClass = new System.Windows.Forms.Label();
+            this.txtClass = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dgvStudents = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblname
+            // 
+            this.lblname.AutoSize = true;
+            this.lblname.Location = new System.Drawing.Point(50, 43);
+            this.lblname.Name = "lblname";
+            this.lblname.Size = new System.Drawing.Size(62, 22);
+            this.lblname.TabIndex = 1;
+            this.lblname.Text = "Họ tên";
+            // 
+            // lblGender
+            // 
+            this.lblGender.AutoSize = true;
+            this.lblGender.Location = new System.Drawing.Point(388, 46);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(81, 22);
+            this.lblGender.TabIndex = 2;
+            this.lblGender.Text = "Giới tính";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(118, 40);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(218, 30);
+            this.txtName.TabIndex = 3;
+            // 
+            // cboGender
+            // 
+            this.cboGender.FormattingEnabled = true;
+            this.cboGender.Items.AddRange(new object[] {
+            "Nam",
+            "Nữ"});
+            this.cboGender.Location = new System.Drawing.Point(494, 43);
+            this.cboGender.Name = "cboGender";
+            this.cboGender.Size = new System.Drawing.Size(99, 30);
+            this.cboGender.TabIndex = 4;
+            this.cboGender.SelectedIndexChanged += new System.EventHandler(this.cboGender_SelectedIndexChanged);
+            // 
+            // lblClass
+            // 
+            this.lblClass.AutoSize = true;
+            this.lblClass.Location = new System.Drawing.Point(679, 51);
+            this.lblClass.Name = "lblClass";
+            this.lblClass.Size = new System.Drawing.Size(42, 22);
+            this.lblClass.TabIndex = 5;
+            this.lblClass.Text = "Lớp";
+            // 
+            // txtClass
+            // 
+            this.txtClass.Location = new System.Drawing.Point(739, 43);
+            this.txtClass.Name = "txtClass";
+            this.txtClass.Size = new System.Drawing.Size(189, 30);
+            this.txtClass.TabIndex = 6;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(42, 121);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(70, 70);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "Thêm";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(173, 121);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(70, 70);
+            this.btnUpdate.TabIndex = 8;
+            this.btnUpdate.Text = "Cập nhật";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(300, 121);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(70, 70);
+            this.btnSearch.TabIndex = 10;
+            this.btnSearch.Text = "Tìm kiếm";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(438, 121);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(70, 70);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "Xoá";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // dgvStudents
+            // 
+            this.dgvStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStudents.Location = new System.Drawing.Point(-40, 253);
+            this.dgvStudents.Name = "dgvStudents";
+            this.dgvStudents.RowHeadersWidth = 51;
+            this.dgvStudents.RowTemplate.Height = 24;
+            this.dgvStudents.Size = new System.Drawing.Size(1385, 354);
+            this.dgvStudents.TabIndex = 11;
+            this.dgvStudents.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvStudents_CellClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1806, 619);
+            this.Controls.Add(this.dgvStudents);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtClass);
+            this.Controls.Add(this.lblClass);
+            this.Controls.Add(this.cboGender);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblGender);
+            this.Controls.Add(this.lblname);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "Quản lý sinh viên";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }   
+
+        #endregion
+        private System.Windows.Forms.Label lblname;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.ComboBox cboGender;
+        private System.Windows.Forms.Label lblClass;
+        private System.Windows.Forms.TextBox txtClass;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dgvStudents;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentManager
+{
+    public partial class Form1 : Form
+    {
+        List<Student> students = new List<Student>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private class Student
+        {
+            public string Name { get; set; }
+            public string Gender { get;set; }
+            public string Class { get; set; }
+        }
+        private void cboGender_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void windowsUIButtonPanel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Student s = new Student
+            {
+                Name = txtName.Text,
+                Gender = cboGender.Text,
+                Class = txtClass.Text
+            };
+
+            students.Add(s);
+
+           
+            dgvStudents.DataSource = null;
+            dgvStudents.DataSource = students;
+        }
+
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if(dgvStudents.CurrentRow != null)
+            {
+                int index = dgvStudents.CurrentRow.Index;
+                students[index].Name = txtName.Text;
+                students[index].Gender = cboGender.Text;
+                students[index].Class = txtClass.Text;
+                dgvStudents.DataSource = null;
+                dgvStudents.DataSource = students;
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if(dgvStudents.CurrentRow !=null)
+            {
+                int index = dgvStudents.CurrentRow.Index;
+                students.RemoveAt(index);
+                dgvStudents.DataSource = null;
+                dgvStudents.DataSource = students;
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string keyword = txtName.Text.ToLower();
+            var results = students.FindAll(s => s.Name.ToLower().Contains(keyword));
+            dgvStudents.DataSource = null;
+            dgvStudents.DataSource = results;
+
+        }
+
+        
+
+        private void dgvStudents_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.RowIndex>=0)
+            {
+                txtName.Text = dgvStudents.Rows[e.RowIndex].Cells[0].Value.ToString();
+                cboGender.Text = dgvStudents.Rows[e.RowIndex].Cells[1].Value.ToString();
+                txtClass.Text = dgvStudents.Rows[e.RowIndex].Cells[2].Value.ToString();
+            }
+        }
+    }
+}
